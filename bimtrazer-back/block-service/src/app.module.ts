a31728ea5a8c0ff96c206e46bc71c902d82a9cc3
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-24 12:01:08 -0300

    Connceted db in block service

--- bimtrazer-back/block-service/src/app.module.ts
+++ bimtrazer-back/block-service/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/block-db')],
   controllers: [AppController],
   providers: [],
 })
