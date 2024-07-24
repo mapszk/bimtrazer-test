b56b0e7869c93a9a66e33bef576aa8b4b5d7cfd8
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-24 18:19:18 -0300

    Created front middleware

--- bimtrazer-front/components/Navbar.tsx
+++ bimtrazer-front/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Container from "./Container";
 export default function Navbar() {
   return (
     <Container>
-      <nav className="flex gap-6 items-center py-10">
+      <nav className="shadow-md rounded-lg flex gap-6 items-center p-4 px-6 my-4 mb-8">
         <Link href="/blocks">Blocks</Link>
         <Link href="/create">New block</Link>
         <button className="ml-auto">Log out</button>
---
+++ bimtrazer-front/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("access_token") || true;
+  const privateRoutes = /\/(create|blocks|edit)/;
+  const isPrivate = privateRoutes.test(request.url);
+
+  if (isPrivate && !token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  if (!isPrivate && token) {
+    return NextResponse.redirect(new URL("/blocks", request.url));
+  }
+}
+
+export const config = {
+  matcher: "/((?!api|static|.*\\..*|_next).*)",
+};
