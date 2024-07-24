986f873021286479eae08e5d968e08517626f0ff
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-24 11:06:47 -0300

    Added microservice structure

--- bimtrazer-back/block-service/src/main.ts
+++ bimtrazer-back/block-service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 8888,
+    },
+  });
+  await app.listen();
 }
 bootstrap();
